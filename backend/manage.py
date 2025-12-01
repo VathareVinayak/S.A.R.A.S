7825e71add939c4733a7dfb207df36c27ad40c9d
VathareVinayak <vinayak.vathare2004@gmail.com>
2025-12-01 22:03:05 +0530

    (backend): added manage.py

---
+++ backend/manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+from django.core.management import execute_from_command_line
+
+# Run administrative tasks.
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saras_backend.settings')
+
+    execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
